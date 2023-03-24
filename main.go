cd2f8d854b1d6425d1a0bfc7a9a6898bea1d6b6c
cerise norling <salineselin@gmail.com>
2023-03-24 15:00:04 -0600

    editing content

--- main.go
+++ main.go
@@ -1,24 +1,51 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"regexp"
 )
 
+type Stuff struct {
+	things []string
+}
+
+type Controller struct {
+	path *regexp.Regexp
+}
+
 func main() {
-	initialize()
+	RegisterControllers()
 	http.ListenAndServe(":8080", nil)
 }
 
-type api struct {
-	input string
-	port  string
+func (c Controller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/api" {
+		switch r.Method {
+		case http.MethodGet:
+			c.Get(w, r)
+		case http.MethodPost:
+			c.Post(w, r)
+		case http.MethodPut:
+			c.Put(w, r)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 }
 
-func initialize() {
-	fmt.Println("initializing api")
+func (c Controller) Get(w http.ResponseWriter, r *http.Request)  {}
+func (c Controller) Post(w http.ResponseWriter, r *http.Request) {}
+func (c Controller) Put(w http.ResponseWriter, r *http.Request)  {}
+
+func newController() *Controller {
+	return &Controller{
+		path: regexp.MustCompile(`^/api`),
+	}
 }
 
-func (api api) get() {}
-
-func (api api) post() {}
+func RegisterControllers() {
+	c := newController()
+	http.Handle("/api", c)
+}
